7d3bfe7072d15a915a1c44b44849793cbd6e0da1
Jamisson Lira Ramos <missonlira@gmail.com>
2024-06-14 16:11:18 -0300

    Retirndo o color do arquivo;

--- src/styles/element/Base.js
+++ src/styles/element/Base.js
@@ -11,7 +11,6 @@ const Base = createGlobalStyle`
 
         font-family: "Roboto", sans-serif;
         font-size: 16px;
-        color: ${Theme.colors.blue800};
         line-height: 24px;
         letter-spacing: 0.5px;
         font-weight: 400;

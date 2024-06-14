526d9aee8fb2985008f39f610a20dad29f6df51f
Jamisson Lira Ramos <missonlira@gmail.com>
2024-06-14 16:08:45 -0300

    Criando component de button;

---
+++ src/components/buttons/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyledButton } from './style';
+
+//variant, color, icon,
+
+ //     
+const ButtonCustom = ({ $variant, color = '#1F7EE6', icon, value, onclick }) => {
+
+    console.log($variant)
+    return (
+        <StyledButton $variant={$variant} color={color} onClick={onclick}>
+            {icon && <span> {icon} </span> }
+            {value}
+        </StyledButton>
+        
+    );
+};
+
+export default ButtonCustom;

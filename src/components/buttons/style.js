f44973645ec4159beb96138984a2cfa465f7b7ee
Jamisson Lira Ramos <missonlira@gmail.com>
2024-06-16 10:58:53 -0300

    Atualizando dados do arquivo e finalizando button;

--- src/components/buttons/style.js
+++ src/components/buttons/style.js
@@ -6,8 +6,8 @@ const containerStyles = css`
     display: flex;
     align-items: center;
     justify-content: center;
-    gap: 16px;
-    padding: 4px 16px;
+    gap: 8px;
+    padding: 8px 16px;
     border-radius: 8px;
     font-size: .8rem;
     font-weight: 500;
@@ -39,13 +39,13 @@ const outlineStyles = css`
     border: 2px solid ${(props) => props.color } ;
     color: ${(props) => props.color };
     &:hover {
-        color: ${Theme.colors.white800};
+        color: ${Theme.colors.white800}; 
         background-color: ${props => {
             const color = props.color;
             const r = parseInt(color.slice(1, 3), 16);
             const g = parseInt(color.slice(3, 5), 16);
             const b = parseInt(color.slice(5, 7), 16);
-            return `rgb(${r - 2}, ${g - 2}, ${b - 2})`;
+            return `rgb(${r - 35}, ${g - 35}, ${b - 35})`;
         }};
     }
 `;
